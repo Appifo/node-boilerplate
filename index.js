1a925cbac0e3978aac49a7a7e682aedfec14bd0a
Appifo <appifodev@gmail.com>
2023-12-09 02:28:31 +0500

    Enabled cors to resolve cors issues

--- index.js
+++ index.js
@@ -4,15 +4,26 @@ import { Server } from "socket.io";
 import mongoose from "mongoose";
 import "dotenv/config";
 import routes from "./src/routes/index.js";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
-const baseUrl = process.env.APP_URL || 'http://localhost';
+const baseUrl = process.env.APP_URL || "http://localhost";
 const port = process.env.PORT || 4000;
 
 // Middlewre.....
 app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 // Sockets.....
 io.on("connection", (socket) => {
@@ -23,10 +34,10 @@ io.on("connection", (socket) => {
 mongoose
   .connect(process.env.MONGO_URI)
   .then((res) => {
-    // Routing.....
     console.log("Database Connected Successfully");
+    // Routing.....
     routes(app);
-    
+
     // Listening for requests.....
     server.listen(port, () => {
       console.log(`server running at ${baseUrl}:${port}`);
